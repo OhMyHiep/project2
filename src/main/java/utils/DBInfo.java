1a6e9bc8e4a065b6e2b03d535c15662ccc53ab55
danscode <dankim.dk1@gmail.com>
2022-06-22 15:21:21 -0400

    updated db stuff

--- src/main/java/utils/DBInfo.java
+++ src/main/java/utils/DBInfo.java
@@ -1,7 +1,7 @@
 package com.beaconfire.utils;
 
 public final class DBInfo {
-	public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String JDBC_DRIVER = "org.postgresql.Driver";
 	public static final String DB_URL = "jdbc://database-project0.c3f2ribjt3t3.us-east-1.rds.amazonaws.com";
 	public static final String DB_NAME = "postgres";
 	public static final String USER = "postgres";
