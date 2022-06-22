c719cbe2e05172406e7664ac4a0251154d7e061c
OhMyHiep <huynhhc@lemoyne.edu>
2022-06-22 17:09:43 -0400

    fill out  BasicCrude interface

--- src/main/java/dao/BasicCrud.java
+++ src/main/java/dao/BasicCrud.java
@@ -1,4 +1,18 @@
 package dao;
 
-public interface BasicCrud {
+import entity.User;
+
+import java.util.List;
+
+public interface BasicCrud <T>{
+
+    public T getById(Integer id);
+    public List<T> getAll();
+    public Integer deleteById(Integer id); //return id of deleted record
+
+    public Integer insert(User user);//return the id of the inserted obj
+
+    public Integer update(Integer id);//return id of updated obj
+
+
 }
