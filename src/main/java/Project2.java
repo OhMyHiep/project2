4d9e272832b51dc1050135e777df994ca7e090e5
s2rostam <33668049+s2rostam@users.noreply.github.com>
2022-06-23 17:03:02 -0400

    Fixed Parject2.java

--- src/main/java/Project2.java
+++ src/main/java/Project2.java
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import dao.BugCommentDao;
 import entity.BugComment;
 import entity.User;
 
 public class Project2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        User user=User.builder().
 //                firstname("Hiep").
 //                lastname("Huynh").
@@ -13,31 +12,5 @@ public class Project2 {
         BugCommentDao bc = new BugCommentDao();
         BugComment test = bc.getById(1);
         System.out.println(test);
-=======
-import com.mitchellbosecke.pebble.PebbleEngine;
-import io.javalin.Javalin;
-import io.javalin.http.staticfiles.Location;
-import io.javalin.plugin.rendering.JavalinRenderer;
-import io.javalin.plugin.rendering.template.JavalinPebble;
-
-public class Project2 {
-    public static void main(String[] args){
-        JavalinPebble.INSTANCE.configure(new PebbleEngine.Builder().build());
-        JavalinRenderer.register(JavalinPebble.INSTANCE, ".html");
-        Javalin app = Javalin.create(
-                config -> {
-                    config.addStaticFiles("src/main/resources/public", Location.EXTERNAL);
-                }
-             );
-        app.start(9090);
-        app.get("/", ctx -> {
-            ctx.redirect("login.html");
-        });
-
-
-
-
->>>>>>> e67ca52 (doing dao)
     }
-
 }
